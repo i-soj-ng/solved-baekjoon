458173fbfc2c605abb9d6fb37912e9e9962e205e
이소정 <jeong4530@gmail.com>
2025-04-23 16:25:40 +0900

    Update 의상.java

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -5,7 +5,9 @@ class Solution {
         int answer = 1;
         
         HashMap<String, Integer> map = new HashMap<>();
-        
+
+        // 의상 종류별로 해당 의상의 개수를 HashMap의 Value로 저장
+        // 해당 종류의 옷을 안 입는 경우도 포함하여 Default 값을 1로 지정
         for(int i = 0; i < clothes.length; i++) {
             String key = clothes[i][1];
             map.put(key, map.getOrDefault(key, 1) + 1);
