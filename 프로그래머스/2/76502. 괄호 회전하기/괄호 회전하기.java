10052e4e562b39f946f6835d4f7ad9cec2799dfa
이소정 <jeong4530@gmail.com>
2025-05-30 13:03:43 +0900

    [level 2] Title: 괄호 회전하기, Time: 27.59 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,7 +1,27 @@
 import java.util.*;
 
 class Solution {
-    public boolean isValid(Deque<Character> deque) {
+    public int solution(String s) {
+        int answer = 0;
+        
+        Deque<Character> deque = new ArrayDeque<>();
+        
+        for (char c : s.toCharArray()) {
+            deque.add(c);
+        }
+        
+        for (int i = 0; i < deque.size(); i++) {
+            if (isValid(deque)) {
+                answer++;
+            }
+            
+            deque.add(deque.poll());
+        }
+        
+        return answer;
+    }
+    
+    boolean isValid(Deque<Character> deque) {
         Deque<Character> stack = new ArrayDeque<>();
         
         for (char c : deque) {
@@ -18,26 +38,7 @@ class Solution {
                 }
             }
         }
+        
         return stack.isEmpty();
-    }
-    
-    public int solution(String s) {
-        int answer = 0;
-        
-        Deque<Character> deque = new ArrayDeque<>();
-        
-        for (char c : s.toCharArray()) {
-            deque.add(c);
-        }
-        
-        for (int i = 0; i < deque.size(); i++) {
-            if (isValid(deque)) {
-                answer += 1;
-            }
-            
-            deque.add(deque.poll());
-        } 
-        
-        return answer;
     }
 }
