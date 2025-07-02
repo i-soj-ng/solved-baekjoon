5a38f8876c86a5283d718c0b88499708f3b5fde4
이소정 <jeong4530@gmail.com>
2025-07-02 17:22:07 +0900

    [level 2] Title: 소수 찾기, Time: 861.47 ms, Memory: 279 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 9.54 MB, 시간: 13.89 ms
+메모리: 279 MB, 시간: 861.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 01일 17:20:28
+2025년 07월 02일 17:22:02
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,6 +1,9 @@
 from itertools import permutations
-
-def solution(numbers):
-    nums = set(int(''.join(p)) for i in range(1, len(numbers)+1)
-               for p in permutations(numbers, i))
-    return sum(n > 1 and all(n % i for i in range(2, int(n**0.5)+1)) for n in nums)
+def solution(n):
+    a = set()
+    for i in range(len(n)):
+        a |= set(map(int, map("".join, permutations(list(n), i + 1))))
+    a -= set(range(0, 2))
+    for i in range(2, int(max(a) ** 0.5) + 1):
+        a -= set(range(i * 2, max(a) + 1, i))
+    return len(a)
