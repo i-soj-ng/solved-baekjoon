59be26b8caa99e28477a3f832f503415730ba39f
이소정 <jeong4530@gmail.com>
2025-07-02 15:38:31 +0900

    [level 2] Title: 타겟 넘버, Time: 340.66 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 48.5 MB, 시간: 200.72 ms
+메모리: 33.7 MB, 시간: 340.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 01일 17:13:28
+2025년 07월 02일 15:38:29
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,20 +1,6 @@
+from itertools import product
+
 def solution(numbers, target):
-  leaves = [0]          # 모든 계산 결과를 담자      
-  count = 0 
-
-  for num in numbers : 
-    temp = []
-	
-    # 자손 노드 
-    for leaf in leaves : 
-      temp.append(leaf + num)    # 더하는 경우 
-      temp.append(leaf - num)    # 빼는 경우 
-
-    leaves = temp 
-
-  # 모든 경우의 수 계산 후 target과 같은지 확인 
-  for leaf in leaves : 
-    if leaf == target : 
-      count += 1
-
-  return count
+    l = [(x, -x) for x in numbers]
+    s = list(map(sum, product(*l)))
+    return s.count(target)
