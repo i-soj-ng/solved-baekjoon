e99a562da47526e1c337203a636ee4cce88d79ef
이소정 <jeong4530@gmail.com>
2025-11-13 18:18:43 +0900

    [level 2] Title: 타겟 넘버, Time: 16.43 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 340.66 ms
+메모리: 35.5 MB, 시간: 16.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 02일 15:38:29
+2025년 11월 13일 18:18:40
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,19 @@
+function solution(numbers, target) {
+    var answer = 0;
+    
+    function dfs(index, sum) {
+        if (index === numbers.length) {
+            if (sum === target)
+                answer++;
+            
+            return;
+        }
+        
+        dfs(index + 1, sum + numbers[index]); 
+        dfs(index + 1, sum - numbers[index]);
+    }
+    
+    dfs(0, 0);
+    
+    return answer;
+}
